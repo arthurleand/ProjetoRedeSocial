a4039b173da784ea13d6d7bee8001f38fe85b1b1
Arthur Leandro <arthurdocsgo@gmail.com>
2021-12-19 15:24:46 -0300

    @arthurleand [Task: ADJUSTMENTS]

--- RedeSocial/src/main/java/com/redesocial/RedeSocial/models/PostModel.java
+++ RedeSocial/src/main/java/com/redesocial/RedeSocial/models/PostModel.java
@@ -39,10 +39,9 @@ public class PostModel {
 	@Size(min = 5, max = 5000)
 	private String descriptionPost;
 
-	@Size(max = 10000)
+
 	private String picture;
 	
-	@Size(max = 10000)
 	private String archive;
 	
 	@Temporal(TemporalType.TIMESTAMP)
